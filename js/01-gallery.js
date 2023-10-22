8fb3fc82608eee747a5beedf665e9cad18444120
VickyVsV <vikusiavsv@gmail.com>
2023-10-22 18:19:40 +0300

    done 1-2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+listEl.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+listEl.addEventListener('click', hadlerClickOpen);
+let instanse;
+
+function createMarkup(arr) {
+  return arr.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`).join("");
+}
+
+function hadlerClickOpen(evt){
+  evt.preventDefault();
+  // if(evt.target === evt.currentTarget){
+  //   return;
+  // }
+
+  const currentImg = evt.target;
+  if(currentImg.tagName === 'IMG'){
+    console.log(currentImg.dataset.source);
+    instanse = basicLightbox.create(
+      `<img src="${currentImg.dataset.source}">`
+    );
+    instanse.show();
+  }
+}
